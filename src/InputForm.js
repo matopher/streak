874d168a899b9c89f6fc5cd2800ee31471417ff2
Matt Woods <matt@mcmkt-matt-mbp.local>
2017-10-22 17:09:37 -0500

    Added futureAnnualSales to State
    * corrected closeRate formula
    * added floor to future customers

--- src/InputForm.js
+++ src/InputForm.js
@@ -5,28 +5,30 @@ class InputForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			monthlyVisitors: 1000,
-			monthlyLeads: 10,
-			monthlySales: 100,
+			monthlyVisitors: 2000,
+			monthlyLeads: 15,
+			monthlyCustomers: 3,
+			monthlySales: 1500,
 			futureVisitors: 0,
 			futureLeads: 0,
+			futureCustomers: 0,
 			futureSales: 0,
-			finalValue: 1
+			futureAnnualSales: 0
 		};
 	}
 
 	onClick(e) {
-		let futureVisitors = _.floor(this.state.monthlyVisitors * 3.5);
+		let futureVisitors = _.floor(this.state.monthlyVisitors * 3.3);
 		this.setState({ futureVisitors });
 		let futureLeads = _.floor(this.state.monthlyLeads * 3.5);
 		this.setState({ futureLeads });
-		let futureSales = _.floor(this.state.monthlySales * 3.5);
+		let closeRate = this.state.monthlyCustomers / this.state.monthlyLeads;
+		let futureCustomers = _.floor(futureLeads * closeRate);
+		this.setState({ futureCustomers });
+		let futureSales = _.floor(this.state.monthlySales * futureCustomers);
 		this.setState({ futureSales });
-		let calc =
-			this.state.monthlyVisitors *
-			this.state.monthlyLeads *
-			this.state.monthlySales;
-		this.setState({ finalValue: calc });
+		let futureAnnualSales = futureSales * 12;
+		this.setState({ futureAnnualSales });
 	}
 
 	render() {
@@ -59,6 +61,18 @@ class InputForm extends Component {
 					<div className="row">
 						<div className="input-field col s12">
 							<input
+								id="monthly-customers"
+								type="number"
+								value={this.state.monthlyCustomers}
+								onChange={e =>
+									this.setState({ monthlyCustomers: e.target.value })}
+							/>
+							<label>How many new customers do you receive a month?</label>
+						</div>
+					</div>
+					<div className="row">
+						<div className="input-field col s12">
+							<input
 								id="monthly-sales"
 								type="number"
 								value={this.state.monthlySales}
@@ -77,12 +91,15 @@ class InputForm extends Component {
 					Forecast
 				</button>
 				<p>
-					You could kicking back with {this.state.futureVisitors} visitors,{' '}
-					{this.state.futureLeads} leads, and {this.state.futureSales} in sales
-					soon!
+					You could kicking back with{' '}
+					{this.state.futureVisitors.toLocaleString()} visitors,{' '}
+					{this.state.futureLeads.toLocaleString()} leads, and{' '}
+					{this.state.futureCustomers.toLocaleString()} new customers each
+					month!
 				</p>
 				<p>
-					That's worth about ${this.state.finalValue}!
+					That's worth about ${this.state.futureAnnualSales.toLocaleString()} in
+					sales over the next 12 months!
 				</p>
 			</div>
 		);
